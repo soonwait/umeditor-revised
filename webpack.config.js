848228308762128cda84f39ca0bf7f53528c25b3
sun_wei <sun_wei@cdv.com>
2019-04-16 10:57:52 +0800

    test

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new CleanWebpackPlugin({
+
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Development',
+            template: './src/index.html',
+            inject: 'head'
+        })
+    ],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    },
+    devServer: {
+        // compress: true,
+        contentBase: './dist',
+        host: '0.0.0.0',
+        port: 8081,
+        disableHostCheck: true
+    }
+};
